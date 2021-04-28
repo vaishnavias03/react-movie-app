bc8506c1fe5efbc40f4443903cf2c823b8d87855
vaishnavi <vaishnavias03@gmail.com>
2021-04-28 17:00:19 +0530

    Commit Changes

--- src/Components/MovieForm.js
+++ src/Components/MovieForm.js
@@ -52,7 +52,17 @@ const MovieForm = () =>{
 
 const Grids = styled.div`
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(4, 1fr);
+
+    
+    @media (max-width: 1100px){
+        grid-template-columns: repeat(2, 1fr);
+
+    }
+    @media (max-width: 600px){
+        grid-template-columns: repeat(1, 1fr);
+
+    }
 
     
 `;
